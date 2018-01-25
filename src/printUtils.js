9142d9b3dac5ad03ee5af1420966db0fcd2c0ca8
chris <chrisrhayden@gmail.com>
2018-01-25 02:44:23 -0800

    printing utils

--- src/printUtils.js
+++ src/printUtils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape  */
 const print = console.log
 
 const printNNL = x => {
@@ -5,7 +6,11 @@ const printNNL = x => {
 }
 
 const format = {
-  bold: '\033[1m'
+  bold: '1',
+  dim: '2',
+  underlined: '4',
+  inverted: '7',
+  hidden: '8',
 }
 
 const colors = {
@@ -25,11 +30,50 @@ const colors = {
   lightblue: '\033[94m',
   lightmagenta: '\033[95m',
   lightcyan: '\033[96m',
-  white: '\033[97',
+  white: '\033[97m',
   reset: '\033[0m'
+}
+
+const colNum = {
+  default: '39',
+  black: '30',
+  red: '31',
+  green: '32',
+  yellow: '33',
+  blue: '34',
+  magenta: '35',
+  cyan: '36',
+  lightgray: '37',
+  darkgray: '90',
+  lightred: '91',
+  lightgreen: '92',
+  lightyellow: '93',
+  lightblue: '94',
+  lightmagenta: '95',
+  lightcyan: '96',
+  white: '97',
+  reset: '0',
+  rstNli: ' \033[0m \n'
+}
+
+const makeColor = (colorStr = null, formatStr = null) => {
+  const color = (colNum[colorStr]) ? colNum[colorStr] : '0'
+
+  const ifFormat = (format[formatStr]) ? format[formatStr] + ';' : ''
+
+  const pColor = '\033[' + ifFormat + color + 'm'
+
+  return pColor
+}
+
+const printCol = (pText, colorStr = null, formatStr = null) => {
+  const outText = makeColor(colorStr, formatStr) + pText + colNum.rstNli
+
+  process.stdout.write(outText)
 }
 
 exports.colors = colors
 exports.format = format
 exports.print = print
+exports.printCol = printCol
 exports.printNNL = printNNL
---
+++ src/printer.js
@@ -0,0 +1,60 @@
+/** The printer
+ */
+
+const R = require('ramda')
+const {
+  printCol
+} = require('./printUtils')
+
+const map = f => x => x.map(f)
+
+const dropFirst = line => line.slice(1)
+
+const dropLast = line => {
+  const stop = line.length - 1
+  return line.slice(0, stop)
+}
+
+const dropFirstLast = R.compose(dropFirst, dropLast)
+
+const splitWord = R.split(' ')
+
+const newLiOrNot = (w, i) => (i && i % 10 === 0) ? w + '\n' : w
+
+const truncateLine = R.compose(
+  R.join(' '), map(newLiOrNot), splitWord)
+
+const shortDropFirst = R.compose(truncateLine, dropFirst)
+
+const normalLine = line => {
+  const colWd = process.stdout.columns
+  return (line.length > colWd) ? shortDropFirst(line) : dropFirst(line)
+}
+
+const printDispatch = line => {
+  switch (R.head(line)) {
+    case '':
+      return null
+    case '#': // title
+      return [line + '\n', 'magenta', 'bold']
+    case '>': // over all description
+      return [line + '\n', 'green']
+    case '-': // description
+      return [normalLine(line), 'default']
+    case '`': // code
+      return [dropFirstLast(line) + '\n', 'default', 'bold']
+  }
+}
+
+/** map through file, sending to right print */
+const parseAndPrintFile = (fileText) => {
+  const fileData = R.split('\n', fileText)
+
+  fileData.forEach(line => {
+    const pLine = printDispatch(line)
+
+    printCol(...pLine)
+  })
+}
+
+exports.parseAndPrintFile = parseAndPrintFile
